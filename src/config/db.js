ee8af91ea57c1edb38a99027c8081d95a87208cd
Chris <chrisdietrich3666@gmail.com>
2024-03-11 17:57:43 -0400

    add migrations

--- src/config/db.js
+++ src/config/db.js
@@ -1,9 +1,25 @@
+//to connect using docker 
+
 import { Sequelize } from "sequelize";
 
+// database here refers to the database service in docker compose
+
 const db = new Sequelize("magno-new", "chris", "chris123", {
-  host: "127.0.0.1",
+  host: "database",
   dialect: "mysql",
-  port: "3307",
+  port: "3306",
 });
 
 export default db;
+
+
+//If I don't want to use Docker I just need to update my port
+// import { Sequelize } from "sequelize";
+
+// const db = new Sequelize("magno-new", "chris", "chris123", {
+//   host: "127.0.0.1",
+//   dialect: "mysql",
+//   port: "3306",
+// });
+
+// export default db;
